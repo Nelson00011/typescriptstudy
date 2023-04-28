2edb9612db9d4ee6a4d1d221a15273731bc4fb72
Ayla Nelson <aylanelson01@gmail.com>
2023-04-28 12:32:14 -0700

    config TODO tsc fx

---
+++ basics.ts
@@ -0,0 +1,31 @@
+// Primitive: number, string, boolean
+// More complex types: arrays, objects
+// Function types, parameters
+
+// Primitives
+let age: number 
+age = 12.1
+
+let userName: string;
+userName = "Phyto"
+
+let isInstructor: boolean;
+isInstructor = true
+
+// More complex types
+//array of strings, booleans, numbers etc
+let hobbies: string[];
+hobbies=['first', 'second', 'third']
+
+let person: {
+    name: string;
+    age: number;
+};
+person = {
+    name: "Max",
+    age: 32
+};
+
+person = {
+    isEmployee: true
+};
